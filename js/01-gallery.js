692fe5bd6240d39fbafb6b6c591ceebd15bf2ea0
Natalka333 <126109559+Natalka333@users.noreply.github.com>
2023-07-07 09:37:02 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
 
-console.log(galleryItems);
-// const galleryEl = document.querySelector('.gallery');
-// console.log(galleryEl);
+function makeGalleryItemsMarkup(items) {
+  return items
+    .map(
+      item => `
+		<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+      />
+    </a>
+  </li>`
+    )
+    .join('');
+}
+
+galleryEl.innerHTML = makeGalleryItemsMarkup(galleryItems);
+
+galleryEl.addEventListener('click', onClickGalleryItem);
+
+function onClickGalleryItem(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `
+    <img src="${event.target.dataset.source}" width="800" height="600">`
+  );
+  instance.show();
+
+  galleryEl.addEventListener('keydown', event => {
+    if (event.code === 'Escape') {
+    }
+    instance.close();
+  });
+}
