49eeb32086fc8c14104cba1f3e076d79710c8af4
Natalka333 <126109559+Natalka333@users.noreply.github.com>
2023-07-08 15:19:03 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
 
-console.log(galleryItems);
+function makeGalleryItemsMarkup(items) {
+  return items
+    .map(({ original, preview, description }) => {
+      return `
+		<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+      />
+    </a>
+  </li>`;
+    })
+    .join('');
+}
+galleryEl.innerHTML = makeGalleryItemsMarkup(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
